aff85f57f357e27e9f5484413cc434d11c1b9393
Walt Impellicceiri <walt@techelevator.com>
2021-09-14 12:39:50 -0400

    M1D2 - hello world.

---
+++ module-1/02_Variables_Data_Types/lecture-student/java/src/main/java/com/techelevator/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.techelevator;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("H");
+        System.out.print("e");
+
+        System.out.println("Hello World");
+
+        System.out.print("Other Stuff");
+    }
+}
