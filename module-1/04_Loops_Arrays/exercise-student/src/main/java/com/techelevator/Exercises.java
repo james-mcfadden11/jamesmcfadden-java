4222b8730469728533bcfad1d077bb6174e22d01
James McFadden <jamesmcfadden111@gmail.com>
2021-09-16 15:04:25 -0400

    Day 4 homework

--- module-1/04_Loops_Arrays/exercise-student/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/exercise-student/src/main/java/com/techelevator/Exercises.java
@@ -10,7 +10,7 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
-		return false;
+		return nums[0] == 6 || nums[nums.length - 1] == 6;
 	}
 
 	/*
@@ -21,7 +21,7 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
-		return false;
+		return nums.length >= 1 && nums[0] == nums[nums.length - 1];
 	}
 
 	/*
@@ -29,7 +29,7 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		return new int[] {3, 1, 4};
 	}
 
 	/*
@@ -40,7 +40,7 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
-		return false;
+		return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
 	}
 
 	/*
@@ -50,7 +50,7 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+		return nums[0] + nums[1] + nums[2];
 	}
 
 	/*
@@ -61,7 +61,7 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+		return new int[] {nums[1], nums[2], nums[0]};
 	}
 
 	/*
@@ -72,7 +72,7 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+		return new int[] {nums[2], nums[1], nums[0]};
 	}
 
 	/*
@@ -83,7 +83,8 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+		int larger = Math.max(nums[0], nums[2]);
+		return new int[] {larger, larger, larger};
 	}
 
 	/*
@@ -94,7 +95,13 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+		if (nums.length == 0) {
+			return 0;
+		} else if (nums.length == 1) {
+			return nums[0];
+		} else {
+			return nums[0] + nums[1];
+		}
 	}
 
 	/*
@@ -105,7 +112,7 @@ public class Exercises {
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+		return new int[] {a[1], b[1]};
 	}
 
 	/*
@@ -116,7 +123,13 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int noOfEvens = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 2 == 0) {
+				noOfEvens++;
+			}
+		}
+		return noOfEvens;
 	}
 
 	/*
@@ -130,7 +143,21 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
 	public int sum13(int[] nums) {
-		return 0;
+		boolean wasPreviousNumberThirteen = false;
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 13) {
+				// dont count it, toggle to true
+				wasPreviousNumberThirteen = true;
+			} else if (wasPreviousNumberThirteen) {
+				// dont count it, toggle back to false
+				wasPreviousNumberThirteen = false;
+			} else {
+				// count it
+				sum += nums[i];
+			}
+		}
+		return sum;
 	}
 
 	/*
@@ -140,6 +167,17 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+		boolean wasPrevNumTwo = false;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 2) {
+				if (wasPrevNumTwo) {
+					return true;
+				}
+				wasPrevNumTwo = true;
+			} else {
+				wasPrevNumTwo = false;
+			}
+		}
 		return false;
 	}
 	
@@ -150,7 +188,12 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
-		return false;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1 || nums[i] == 3) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/*
@@ -160,7 +203,13 @@ public class Exercises {
 	 sum28([1, 2, 3, 4]) → false
 	 */
 	public boolean sum28(int[] nums) {
-		return false;
+		int sumTwos = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 2) {
+				sumTwos += 2;
+			}
+		}
+		return sumTwos == 8;
 	}
 
 }
