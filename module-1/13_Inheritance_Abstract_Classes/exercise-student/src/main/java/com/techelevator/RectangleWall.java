dafe94d40b5bd126926e3f332c1da4baa7ce019a
James McFadden <jamesmcfadden111@gmail.com>
2021-09-29 13:22:20 -0400

    Day 13 homework

---
+++ module-1/13_Inheritance_Abstract_Classes/exercise-student/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,30 @@
+package com.techelevator;
+
+public class RectangleWall extends Wall {
+    private int length;
+    private int height;
+
+    public RectangleWall(String name, String color, int length, int height) {
+        super(name, color);
+        this.length = length;
+        this.height = height;
+    }
+
+    @Override
+    public int getArea() {
+        return this.length * this.height;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + " (" + this.length + "x" + this.height + ") rectangle";
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
