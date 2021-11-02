5d5e353b81a9ed43cce098aeea9fbdb5fc7ef8a1
Walt Impellicceiri <walt@techelevator.com>
2021-11-02 08:54:03 -0400

    M2D17 - review lecture code.

---
+++ module-2/17_Review/lecture-student/src/main/java/com/techelevator/dao/MovieDao.java
@@ -0,0 +1,31 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Movie;
+
+import java.util.List;
+
+public interface MovieDao {
+
+    Movie create(Movie movie);
+
+    Movie getMovieById(int movieId);
+
+    List<Movie> getAll();
+
+    void update(Movie movie);
+
+    void delete(int movieId);
+
+    List<Movie> getAllByDirector(Integer directorId);
+
+    List<Movie> getAllActedIn(int actorId);
+
+    int getTotalRuntimeOfCollection(String collectionName);
+
+    double getAverageRunTimeByDirector(int directorId);
+
+    Movie getRandomByDirector(int directorId);
+    Movie getRandomByGenre(String genreName);
+
+    int getTimesCollaborated(int directorId, int actorId);
+}
