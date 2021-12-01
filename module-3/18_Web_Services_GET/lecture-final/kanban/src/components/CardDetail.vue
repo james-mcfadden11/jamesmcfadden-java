747355174a8e7f7af9e5d8424874b66dbb936af3
Walt Impellicceiri <walt@techelevator.com>
2021-12-01 12:23:02 -0500

    M3D16 - lecture final.

---
+++ module-3/18_Web_Services_GET/lecture-final/kanban/src/components/CardDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>{{ card.title }}</h1>
+    <p>{{ card.description }}</p>
+
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/ping_pong_loader.gif" />
+    </div>
+    <comments-list v-bind:comments="card.comments" v-else />
+  
+    <div class="board-actions" v-if="!isLoading">
+      <router-link
+        v-bind:to="{ name: 'Board', params: { id: $route.params.boardID } }"
+        >Back to Board</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import boardService from '@/services/BoardService';
+import CommentsList from '@/components/CommentsList.vue';
+
+export default {
+  name: "card-detail",
+  
+  components: {
+    CommentsList
+  },
+
+  data() {
+    return {
+      card: {
+        title: '',
+        description: '',
+        status: '',
+        comments: []
+      },
+
+      isLoading: true
+    }
+  },
+
+  created() {
+    boardService
+      .getCard(this.$route.params.boardID, this.$route.params.cardID)
+      .then((card) => {
+        this.card = card;
+        this.isLoading = false;
+      })
+  }
+};
+</script>
