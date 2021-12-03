1b3f6b1a186e9751e5803524ed306265cd641925
Walt Impellicceiri <walt@techelevator.com>
2021-12-03 09:26:35 -0500

    M3 - final capstone starter code review.

---
+++ module-3/20_Review/lecture-final/java/src/main/java/com/techelevator/model/Person.java
@@ -0,0 +1,26 @@
+package com.techelevator.model;
+
+import javax.validation.constraints.NotNull;
+
+public class Person {
+    private int id;
+
+    @NotNull
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ module-3/20_Review/lecture-final/vue/src/views/Person.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ $store.state.person.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch('GET_PERSON', 1);
+  }
+
+}
+</script>
